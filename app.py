3f4f780e24b1aca5946cc1c1eb6a30fbbd4e6178
Ainash Toleu <ainashtoleu@Ainashs-MacBook-Air.local>
2021-03-22 13:11:25 -0500

    home page added

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 # 1. import Flask
-from flask import Flask
+from flask import Flask, jsonify
 
 # 2. Create an app, being sure to pass __name__
 app = Flask(__name__)
+
+
+# 3. Define what to do when a user hits the index route
+@app.route("/")
+def home():
+    return (
+        f"Welcome to 'Home' page!<br/>"
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+        
+
 
 
 
